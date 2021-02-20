b89f345ecce77a64d3173fbd841d96ea7fe3b53b
Tristan Elliott <>
2021-02-19 21:30:15 -0400

    Employee model

--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,5 +1,42 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
+	
+	private @Id @GeneratedValue Long id;
+	private String name;
+	private String role;
+	
+	Employee(){}
+	
+	Employee(String name,String role){
+		this.name = name;
+		this.role = role;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public String getRole() {
+		return this.role;
+	}
+	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
