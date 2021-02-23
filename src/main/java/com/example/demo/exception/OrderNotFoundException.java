2ed2b6347d0b43db883d3d8f5a0013a56c5239a4
Tristan Elliott <>
2021-02-22 21:22:08 -0400

    order not found exception

--- src/main/java/com/example/demo/exception/OrderNotFoundException.java
+++ src/main/java/com/example/demo/exception/OrderNotFoundException.java
@@ -1,5 +1,11 @@
 package com.example.demo.exception;
 
-public class OrderNotFoundException {
+import java.text.MessageFormat;
+
+public class OrderNotFoundException extends RuntimeException{
+	
+	public OrderNotFoundException(Long id) {
+		super(MessageFormat.format("Could not find order with id:{0}", id));
+	}
 
 }
