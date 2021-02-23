5737e0c7a457e2b33ec4e4a39692d6c62b364b7d
Tristan Elliott <>
2021-02-22 21:39:40 -0400

    adding item services

--- src/main/java/com/example/demo/exception/ItemNotFoundException.java
+++ src/main/java/com/example/demo/exception/ItemNotFoundException.java
@@ -2,9 +2,9 @@ package com.example.demo.exception;
 
 import java.text.MessageFormat;
 
-public class OrderNotFoundException extends RuntimeException{
+public class ItemNotFoundException extends RuntimeException{
 	
-	public OrderNotFoundException(Long id) {
+	public ItemNotFoundException(Long id) {
 		super(MessageFormat.format("Could not find order with id:{0}", id));
 	}
 
--- src/main/java/com/example/demo/services/ItemService.java
+++ src/main/java/com/example/demo/services/ItemService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.ItemNotFoundException;
 import com.example.demo.models.Item;
 import com.example.demo.repository.ItemRepository;
 
@@ -26,7 +27,18 @@ public class ItemService {
 	}
 	public Item getItem(Long id) {
 		return itemRepository.findById(id)
-				.orElseThrow(()-> )
+				.orElseThrow(()->new ItemNotFoundException(id) );
+	}
+	public Item deleteItem(Long id) {
+		Item item = getItem(id);
+		itemRepository.delete(item);
+		return item;
+	}
+	
+	public Item editItem(Item item,Long id) {
+		Item itemToEdit = getItem(id);
+		itemToEdit.setName(item.getName());
+		return itemToEdit;
 	}
 
 }
